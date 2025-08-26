53a2c6362abb991699f126b135b10e8e37aaf3da
Steven Fawcett <steven.fawcett@me.com>
2025-08-26 19:11:01 +0100

    Allow for the Manager interface to return a list of MenuItems and the main form will present and call them

--- Update.cs
+++ Update.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using UpdatePlugin.Properties;
 using UpdatePlugin.Forms;
+using BroadcastPluginSDK;
 
 namespace UpdatePlugin
 {
@@ -12,19 +13,43 @@ namespace UpdatePlugin
 
         private const string Stanza = "YOUR CONFIG STANZA";
         private ILogger<IPlugin> _logger;
+        private static UpdateForm? _formInstance;
+        public List<ToolStripItem>? ContextMenuItems { get; set; } = null;
 
         public UpdatePlugin(IConfiguration configuration, ILogger<IPlugin> logger , IPluginRegistry registry ) :
-            base(configuration, new UpdateForm( logger , registry , configuration  ), Resources.icon, Stanza)
+            base(configuration, CreateForm( logger , registry , configuration  ), Resources.icon, Stanza)
         {
             _logger = logger;
+
+            ContextMenuItems = new List<ToolStripItem>()
+            {
+                new ToolStripMenuItem("Open", null, OnOpenClicked),
+                new ToolStripSeparator(),
+                new ToolStripSeparator(),
+                new ToolStripSeparator(),
+                new ToolStripSeparator(),
+            };
+        }
+
+        private static UpdateForm CreateForm(ILogger<IPlugin> logger, IPluginRegistry registry, IConfiguration config)
+        {
+            _formInstance = new UpdateForm(logger, registry, config);
+            return _formInstance;
         }
 
         public event EventHandler<bool>? TriggerRestart;
+        public event EventHandler<UserControl>? ShowScreen;
 
         public void RequestRestart(bool isForced)
         {
             _logger.LogDebug("Requesting application restart. Forced: {IsForced}", isForced);
             TriggerRestart?.Invoke(this, isForced);
+        }
+
+        private void OnOpenClicked(object? sender, EventArgs e)
+        {
+            if(_formInstance != null)
+                ShowScreen?.Invoke(this, _formInstance  );
         }
     }
 }
